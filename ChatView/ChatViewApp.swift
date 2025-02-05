d39f7c2038d6b99a560400160dc04a5dcc6d75f4
Luis Amorim <luis@amorim.bio.br>
2025-02-05 16:59:39 -0300

    Initial Commit

---
+++ ChatView/ChatViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewApp.swift
+//  ChatView
+//
+//  Created by Luis Amorim on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
